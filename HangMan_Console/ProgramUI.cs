77a637f3fe04a738943e402b506af3ca2dc6ea6a
Dean Collier <daccollier@gmail.com>
2021-07-01 19:43:52 -0400

    initial commit with some classes

---
+++ HangMan_Console/ProgramUI.cs
@@ -0,0 +1,21 @@
+﻿using HangMan_Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan_Console
+{
+    class ProgramUI
+    {
+        private bool isRunning = true;
+        private readonly GetWord _getWord = new GetWord();
+
+        private void Run()
+        {
+
+        }
+
+    }
+}
---
+++ HangMan_Repository/GetWord.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan_Repository
+{
+    public class GetWord
+    {
+        public WordOption WordChoice { get; set; }
+
+        public GetWord(WordOption wordChoice)
+        {
+            WordChoice = wordChoice;
+        }
+        public GetWord() { }
+    }
+    public enum WordOption
+    {
+        day = 1,
+        distance,
+        park,
+        invent,
+        raise,
+        magic,
+        trains,
+        ban,
+        window,
+        drown,
+        ambiguous,
+        babies,
+        deserve,
+        actor,
+        loving,
+        narrow,
+        hushed,
+        pine,
+        dogs,
+        acrid,
+        button,
+        flagrant,
+        whirl,
+        attach,
+        guard,
+        suggest,
+        plausible,
+        power,
+        piquant,
+        boil,
+        kindhearted,
+        sip,
+        unruly,
+        marry,
+        rainy,
+        nondescript,
+        dark,
+        foregoing,
+        income,
+        extralarge,
+        card,
+        wine,
+        sponge,
+        business,
+        reaction,
+        white,
+        erratic,
+        sky,
+        extrasmall,
+        relation,
+        encouraging,
+        cracker,
+        lacking,
+        splendid,
+        gullible,
+        relax,
+        wacky,
+        fasten,
+        sassy,
+        mass,
+        unhealthy,
+        lumber,
+        arm,
+        run,
+        judge,
+        flawless,
+        maid,
+        launch,
+        lie,
+        care,
+        poke,
+        omniscient,
+        colorful,
+        previous,
+        ski,
+        waves,
+        spare,
+        shy,
+        coor,
+        dinated,
+        lake,
+        unbiased,
+        complete,
+        thin,
+        separate,
+        range,
+        comb,
+        absurd,
+        defeated,
+        grieving,
+        half,
+        stone,
+        sprout,
+        giddy,
+        sweet,
+        suspend,
+        rampant,
+        reward,
+        eggs,
+        vacation,
+        brake
+    }
+    
+}
